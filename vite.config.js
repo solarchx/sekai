bea088b6c8d2a7bf1ac4d15dfbf344861f875aa5
solarchx <umwtfamidoing@gmail.com>
2026-02-10 11:29:59 +0700

    Changed Vite config to work with my fucked up domain

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  server: {
-    cors: {
-      origin: [/^https?:\/\/.*\.test(:\d+)?$/], // allow all .test domains
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    server: {
+        cors: {
+            origin: [
+                /^https?:\/\/(.+\.)?(sekai\.sh|localhost)(:\d+)?$/,
+            ],
+        },
     },
-  },
 });

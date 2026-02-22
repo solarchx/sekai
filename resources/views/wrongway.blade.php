1c8d3378ea96afa07d39e8b20f8e171cb0fa64f0
solarchx <umwtfamidoing@gmail.com>
2026-02-22 09:22:19 +0700

    PLEASE SPEED I NEED THIS (2)

--- resources/views/wrongway.blade.php
+++ resources/views/wrongway.blade.php
@@ -4,24 +4,35 @@
             {{ __('Wrong Way') }}
         </h2>
     </x-slot>
-
+    <audio autoplay loop class="hidden" id="plaudite">
+        <source src="{{ asset('plaudite_interlude.mp3') }}" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+    <script>
+        let audio = document.getElementById('plaudite');
+        audio.volume = 0.1;
+    </script>
     <div class="py-12">
-        <div class="relative bg-gray-800 h-96 rounded-lg overflow-hidden">
+        <div class="max-w-7xl min-h-screen mx-auto sm:px-6 lg:px-8">
 
-    <!-- Left Lock -->
-    <img src="{{ asset('img/storylock/lock_side_closed.png') }}"
-         class="absolute top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 w-32 md:w-48"
-         alt="Left Lock">
+            <div class="relative bg-gray-800 rounded-xl shadow-xl">
 
-    <!-- Center Lock -->
-    <img src="{{ asset('img/storylock/lock_main_closed.png') }}"
-         class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-40 md:w-56"
-         alt="Center Lock">
+            <!-- Left Lock -->
+            <img src="{{ asset('img/storylock/lock_side_closed.png') }}"
+                 class="absolute top-1/2 left-[25%] -translate-x-1/2 -translate-y-1/2 w-40 md:w-52"
+                 alt="Left Lock">
 
-    <!-- Right Lock -->
-    <img src="{{ asset('img/storylock/lock_side_closed.png') }}"
-         class="absolute top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2 w-32 md:w-48"
-         alt="Right Lock">
+            <!-- Center Lock -->
+            <img src="{{ asset('img/storylock/lock_main_closed.png') }}"
+                 class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-48 md:w-64"
+                 alt="Center Lock">
+
+            <!-- Right Lock -->
+            <img src="{{ asset('img/storylock/lock_side_closed.png') }}"
+                 class="absolute top-1/2 left-[75%] -translate-x-1/2 -translate-y-1/2 w-40 md:w-52"
+                 alt="Right Lock">
+
+            </div>
         </div>
-    </div>
+    </div>  
 </x-app-layout>
